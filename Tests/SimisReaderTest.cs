b8556f0e0f223be74f0152fdff42f27ceb4520a0
JamesRoss_cp <SND\JamesRoss_cp>
2010-01-07 22:25:38 +0000

    Add list of MSTS files (to be put in "Microsoft Train Simulator" directory if you want to use the test). Added SimisReaderTest (unimplemented) that uses the file list.

---
+++ Tests/SimisReaderTest.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests {
+	/// <summary>
+	/// Summary description for SimisReaderTest
+	/// </summary>
+	[TestClass]
+	public class SimisReaderTest {
+		private TestContext testContextInstance;
+
+		/// <summary>
+		///Gets or sets the test context which provides
+		///information about and functionality for the current test run.
+		///</summary>
+		public TestContext TestContext {
+			get {
+				return testContextInstance;
+			}
+			set {
+				testContextInstance = value;
+			}
+		}
+
+		/// <summary>
+		///A test for SimisReader on real files
+		///</summary>
+		[DeploymentItem("Tests\\MSTS files.csv"), DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "|DataDirectory|\\MSTS files.csv", "MSTS files#csv", DataAccessMethod.Sequential), TestMethod]
+		public void TestRealFiles() {
+			Assert.Fail((string)TestContext.DataRow["Filename"]);
+		}
+	}
+}
