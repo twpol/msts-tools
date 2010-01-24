4b4b28f29c047940be8943c01e71409e01b564c3
James Ross <twpol@users.noreply.github.com>
2010-01-24 20:46:49 +0000

    New project - JGR.MSTS - for MSTS-specific classes like routes and activities, which will build on and use the SimisFile and related classes inside. Jgr.Msts.Route class: given a route directory, provides access to basic information about the route (to be expanded).

---
+++ JGR.MSTS/Route.cs
@@ -0,0 +1,41 @@
+﻿//------------------------------------------------------------------------------
+// Jgr.IO.Parser library, part of MSTS Editors & Tools (http://jgrmsts.codeplex.com/).
+// License: Microsoft Public License (Ms-PL).
+//------------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Jgr.IO.Parser;
+using System.IO;
+
+namespace Jgr.Msts {
+	public class Route {
+		public string RootPath { get; private set; }
+		SimisProvider SimisProvider;
+		SimisFile TrackFile;
+
+		public Route(string path, SimisProvider simisProvider) {
+			RootPath = path;
+			SimisProvider = simisProvider;
+
+			var tracks = Directory.GetFiles(path, "*.trk", SearchOption.TopDirectoryOnly).Where(n => n.EndsWith(".trk", StringComparison.OrdinalIgnoreCase));
+			if (tracks.Count() != 1) throw new ArgumentException("Path contains " + tracks.Count() + " .trk files; must be exactly 1.", "path");
+			TrackFile = new SimisFile(tracks.First(), SimisProvider);
+			TrackFile.ReadFile();
+		}
+
+		public string Name {
+			get {
+				return TrackFile.Tree["Tr_RouteFile"]["Name"][0].ToValue<string>();
+			}
+		}
+
+		public string Description {
+			get {
+				return TrackFile.Tree["Tr_RouteFile"]["Description"][0].ToValue<string>().Replace("\n", "\r\n");
+			}
+		}
+	}
+}
