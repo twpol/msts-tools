d1177aa3b3c84b17951d2382ecb9550a16f08bdb
James Ross <twpol@users.noreply.github.com>
2010-05-31 15:48:43 +0000

    Mark and re-work SimisProvider to be immutable.

--- JGR.IO.Parser/SimisProvider.cs
+++ JGR.IO.Parser/SimisProvider.cs
@@ -25,67 +25,38 @@ namespace Jgr.IO.Parser {
 	/// <summary>
 	/// Loads and stores data necessary for processing Simis files: token name-id mappings and all the <see cref="SimisFormat"/>s.
 	/// </summary>
+	[Immutable]
 	public class SimisProvider {
 		/// <summary>
 		/// Provides a mapping of token IDs (as <see cref="uint"/>) to token names (as <see cref="string"/>).
 		/// </summary>
-		public Dictionary<uint, string> TokenNames { get; protected set; }
+		public IDictionary<uint, string> TokenNames { get; private set; }
 		/// <summary>
 		/// Provides a mapping of token names (as <see cref="string"/>) to token IDs (as <see cref="uint"/>).
 		/// </summary>
-		public Dictionary<string, uint> TokenIds { get; protected set; }
+		public IDictionary<string, uint> TokenIds { get; private set; }
 		/// <summary>
 		/// Provides a collection of <see cref="SimisFormat"/>s which have been loaded.
 		/// </summary>
-		public List<SimisFormat> Formats { get; protected set; }
-		Thread BackgroundLoader;
-		Exception LoadError;
+		public IEnumerable<SimisFormat> Formats { get; private set; }
 
-		protected SimisProvider() {
-			TokenNames = new Dictionary<uint, string>();
-			TokenIds = new Dictionary<string, uint>();
-			Formats = new List<SimisFormat>();
-		}
+		public SimisProvider(string directory) {
+			var tokenNames = new Dictionary<uint, string>();
+			var tokenIds = new Dictionary<string, uint>();
+			var formats = new List<SimisFormat>();
 
-		public SimisProvider(string directory) : this() {
-			BackgroundLoader = new Thread(() => BackgroundLoad(directory));
-			BackgroundLoader.Start();
-		}
-
-		public void Join() {
-			BackgroundLoader.Join();
-			if (LoadError != null) throw LoadError;
-		}
-
-		/// <summary>
-		/// Gets the subset of <see cref="SimisFormat"/>s appropriate for a given filename.
-		/// </summary>
-		/// <param name="fileName">The filename with which to restrict the formats. Does not need the path.</param>
-		/// <returns>A <see cref="SimisProvider"/> which only has <see cref="SimisFormat"/>s appropriate for the given filename.</returns>
-		public SimisProvider GetForPath(string fileName) {
-			return new SimisProviderForFile(this, fileName);
-		}
-
-		public SimisFormat GetForFormat(string format) {
-			return Formats.FirstOrDefault(f => f.Format == format);
-		}
-
-		void BackgroundLoad(string directory) {
 			foreach (var filename in Directory.GetFiles(directory, "*.bnf")) {
 				var BNF = new BnfFile(filename);
 				try {
 					BNF.ReadFile();
-				} catch (FileException e) {
-					LoadError = e;
-					return;
 				} catch (InvalidDataException) {
 					// BNF didn't specify all required stuff, skip it.
 					continue;
 				}
 				var simisFormat = new SimisFormat(BNF);
-				Formats.Add(simisFormat);
+				formats.Add(simisFormat);
 			}
-			Formats.Sort((a, b) => StringComparer.InvariantCultureIgnoreCase.Compare(a.Name, b.Name));
+			formats.Sort((a, b) => StringComparer.InvariantCultureIgnoreCase.Compare(a.Name, b.Name));
 
 			foreach (var filename in Directory.GetFiles(directory, "*.tok")) {
 				var ffReader = new StreamReader(System.IO.File.OpenRead(filename), Encoding.ASCII);
@@ -98,12 +69,53 @@ namespace Jgr.IO.Parser {
 						tokenIndex = 0x0000;
 					} else if (ffLine.StartsWith("SIDDEF(", StringComparison.Ordinal)) {
 						var name = ffLine.Substring(ffLine.IndexOf('"') + 1, ffLine.LastIndexOf('"') - ffLine.IndexOf('"') - 1);
-						TokenNames.Add(((uint)tokenType << 16) + ++tokenIndex, name);
-						TokenIds.Add(name, ((uint)tokenType << 16) + tokenIndex);
+						tokenNames.Add(((uint)tokenType << 16) + ++tokenIndex, name);
+						tokenIds.Add(name, ((uint)tokenType << 16) + tokenIndex);
 					}
 					if (ffReader.EndOfStream) break;
 				}
 			}
+
+			TokenNames = tokenNames;
+			TokenIds = tokenIds;
+			Formats = formats;
+		}
+
+		internal SimisProvider(IDictionary<uint, string> tokenNames, IDictionary<string, uint> tokenIds, IEnumerable<SimisFormat> formats) {
+			TokenNames = tokenNames;
+			TokenIds = tokenIds;
+			Formats = formats;
+		}
+
+		/// <summary>
+		/// Gets the subset of <see cref="SimisFormat"/>s appropriate for a given filename.
+		/// </summary>
+		/// <param name="fileName">The filename with which to restrict the formats. Does not need the path.</param>
+		/// <returns>A <see cref="SimisProvider"/> which only has <see cref="SimisFormat"/>s appropriate for the given filename.</returns>
+		public SimisProvider GetForPath(string fileName) {
+			var tokenNames = new Dictionary<uint, string>(TokenNames);
+			var tokenIds = new Dictionary<string, uint>(TokenIds);
+			var formats = new List<SimisFormat>();
+			var extension = "";
+
+			var inputFileName = Path.GetFileName(fileName);
+			var inputExtension = Path.GetExtension(fileName);
+
+			// We allow extensions (e.g. "trk") and filenames (e.g. "tsection.dat") in SimisFormat.Extension. If there is a filename match, we should
+			// use that and only that; otherwise, any extension matches are in.
+			if (Formats.Any(f => inputFileName.Equals(f.Extension, StringComparison.OrdinalIgnoreCase))) {
+				formats = new List<SimisFormat>(Formats.Where(f => inputFileName.Equals(f.Extension, StringComparison.OrdinalIgnoreCase)));
+				extension = inputFileName;
+			} else {
+				formats = new List<SimisFormat>(Formats.Where(f => inputExtension.Equals("." + f.Extension, StringComparison.OrdinalIgnoreCase)));
+				extension = inputExtension;
+			}
+
+			return new SimisProviderForFile(tokenNames, tokenIds, formats, extension);
+		}
+
+		public SimisFormat GetForFormat(string format) {
+			return Formats.FirstOrDefault(f => f.Format == format);
 		}
 
 		public override string ToString() {
@@ -114,24 +126,13 @@ namespace Jgr.IO.Parser {
 	/// <summary>
 	/// A <see cref="SimisProvider"/> which contains only the subset of Simis formats appropriate for a given filename.
 	/// </summary>
+	[Immutable]
 	public class SimisProviderForFile : SimisProvider {
-		string Extension;
+		public readonly string Extension;
 
-		internal SimisProviderForFile(SimisProvider baseProvider, string fileName) : base() {
-			var inputFileName = Path.GetFileName(fileName);
-			var inputExtension = Path.GetExtension(fileName);
-
-			TokenNames = baseProvider.TokenNames;
-			TokenIds = baseProvider.TokenIds;
-			// We allow extensions (e.g. "trk") and filenames (e.g. "tsection.dat") in SimisFormat.Extension. If there is a filename match, we should
-			// use that and only that; otherwise, any extension matches are in.
-			if (baseProvider.Formats.Any(f => inputFileName.Equals(f.Extension, StringComparison.OrdinalIgnoreCase))) {
-				Formats = new List<SimisFormat>(baseProvider.Formats.Where(f => inputFileName.Equals(f.Extension, StringComparison.OrdinalIgnoreCase)));
-				Extension = inputFileName;
-			} else {
-				Formats = new List<SimisFormat>(baseProvider.Formats.Where(f => inputExtension.Equals("." + f.Extension, StringComparison.OrdinalIgnoreCase)));
-				Extension = inputExtension;
-			}
+		internal SimisProviderForFile(IDictionary<uint, string> tokenNames, IDictionary<string, uint> tokenIds, IEnumerable<SimisFormat> formats, string extension)
+			: base(tokenNames, tokenIds, formats) {
+			Extension = extension;
 		}
 
 		public override string ToString() {
