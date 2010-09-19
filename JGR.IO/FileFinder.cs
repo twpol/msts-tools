861d019bc682e7d6db00b33400e645bbf8def7f6
JamesRoss_cp <SND\JamesRoss_cp>
2010-09-19 14:45:28 +0000

    Documentation for some library classes.

--- JGR.IO/FileFinder.cs
+++ JGR.IO/FileFinder.cs
@@ -8,14 +8,27 @@ using System.Collections.Generic;
 using System.IO;
 
 namespace Jgr.IO {
+	/// <summary>
+	/// Stores a list of file paths within which files are to be found.
+	/// </summary>
 	[Immutable]
 	public class FileFinder {
 		public IEnumerable<string> Paths { get; private set; }
 
+		/// <summary>
+		/// Constructs the <see cref="FileFinder"/> with a set of <paramref name="paths"/> to search.
+		/// </summary>
+		/// <param name="paths">The sequence of paths to search for files in.</param>
 		public FileFinder(IEnumerable<string> paths) {
 			Paths = paths;
 		}
 
+		/// <summary>
+		/// Returns the first path found containing the <paramref name="fileName"/>.
+		/// </summary>
+		/// <param name="fileName">The file to find in the paths. May contain directory elements, including up-level.</param>
+		/// <returns>The file path of the found file.</returns>
+		/// <exception cref="ArgumentException">Thrown if the file is not found in any of the paths.</exception>
 		public string this[string fileName] {
 			get {
 				foreach (var path in Paths) {
--- JGR/LinqExtensions.cs
+++ JGR/LinqExtensions.cs
@@ -7,23 +7,59 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace Jgr {
+	/// <summary>
+	/// Provides a number of useful extensions to .NET 3.5's LINQ methods.
+	/// </summary>
 	public static class LinqExtensions {
+		/// <summary>
+		/// Returns an <see cref="IEnumerable{T}"/> containing only <paramref name="self"/>.
+		/// </summary>
+		/// <typeparam name="T">The type of the element of <paramref name="self"/>.</typeparam>
+		/// <param name="self">The object to return an enumeration containing.</param>
+		/// <returns>An <see cref="IEnumerable{T}"/> containing only <paramref name="self"/>.</returns>
 		public static IEnumerable<T> AsEnumerable<T>(this T self) {
 			return new T[] { self };
 		}
 
+		/// <summary>
+		/// Prefixes a sequence with another sequence.
+		/// </summary>
+		/// <typeparam name="T">The type of the elements of the input sequences.</typeparam>
+		/// <param name="second">The sequence to prefix.</param>
+		/// <param name="first">The sequence being prefixed to the <paramref name="second"/> sequence.</param>
+		/// <returns>As <see cref="IEnumerable{T}"/> containing both sequences in <paramref name="first"/>, <paramref name="second"/> order.</returns>
 		public static IEnumerable<T> Prefix<T>(this IEnumerable<T> second, IEnumerable<T> first) {
 			return first.Concat(second);
 		}
 
+		/// <summary>
+		/// Prefixes a sequence with a single item.
+		/// </summary>
+		/// <typeparam name="T">The type of the elements of the input sequence.</typeparam>
+		/// <param name="source">The sequence to prefix.</param>
+		/// <param name="item">The element being prefixed to the <paramref name="source"/> sequence.</param>
+		/// <returns>As <see cref="IEnumerable{T}"/> containing <paramref name="item"/> followed by the <paramref name="source"/> sequence.</returns>
 		public static IEnumerable<T> Prefix<T>(this IEnumerable<T> source, T item) {
 			return source.Prefix(new T[] { item });
 		}
 
+		/// <summary>
+		/// Concatenates a sequence with a single item.
+		/// </summary>
+		/// <typeparam name="T">The type of the elements of the input sequence.</typeparam>
+		/// <param name="source">The sequence to concatenate.</param>
+		/// <param name="item">The element being concatenated to the <paramref name="source"/> sequence.</param>
+		/// <returns>As <see cref="IEnumerable{T}"/> containing the <paramref name="source"/> sequence followed by <paramref name="item"/>.</returns>
 		public static IEnumerable<T> Concat<T>(this IEnumerable<T> source, T item) {
 			return source.Concat(new T[] { item });
 		}
 
+		/// <summary>
+		/// Converts a sequence into a <see cref="Stack{T}"/>.
+		/// </summary>
+		/// <typeparam name="T">The type of the elements of the input sequence.</typeparam>
+		/// <param name="source">The sequence to convert.</param>
+		/// <returns>A <see cref="Stack{T}"/> containing the elements in the <paramref name="source"/> sequence.</returns>
 		public static Stack<T> AsStack<T>(this IEnumerable<T> source) {
 			return new Stack<T>(source.Reverse());
 		}
