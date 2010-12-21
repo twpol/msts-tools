52f06f679b9b12acf700bb49053c6e68ecf0511d
JamesRoss_cp <SND\JamesRoss_cp>
2010-12-21 15:37:39 +0000

    Added caching support to CodePlexVersionCheck. Added update check to Tools window.

--- JGR/CodePlexVersionCheck.cs
+++ JGR/CodePlexVersionCheck.cs
@@ -20,12 +20,14 @@ namespace Jgr
 	{
 		public event EventHandler CheckComplete;
 		public bool HasLatestVersion { get; private set; }
+		public DateTime LatestVersionDate { get; private set; }
 		public string LatestVersionTitle { get; private set; }
 		public Uri LatestVersionUri { get; private set; }
 		public bool IsNewVersion { get; private set; }
-		Uri Feed;
-		string ReleaseName;
-		DateTime CurrentVersionReleaseDate;
+		readonly ApplicationSettings Settings;
+		readonly Uri Feed;
+		readonly string ReleaseName;
+		readonly DateTime CurrentVersionReleaseDate;
 
 		/// <summary>
 		/// Initializes the CodePlex check.
@@ -37,25 +39,55 @@ namespace Jgr
 			Feed = new Uri("http://" + projectName + ".codeplex.com/Project/ProjectRss.aspx?ProjectRSSFeed=" + Uri.EscapeDataString("codeplex://release/" + projectName), UriKind.Absolute);
 			ReleaseName = releaseName;
 			CurrentVersionReleaseDate = currentVersionReleaseDate;
-			HasLatestVersion = false;
-			LatestVersionTitle = "";
-			LatestVersionUri = null;
-			IsNewVersion = false;
+		}
+
+		/// <summary>
+		/// Initializes the CodePlex check.
+		/// </summary>
+		/// <param name="settings">The <see cref="ApplicationSettings"/> to read, write and cache update checks via.</param>
+		/// <param name="projectName">The name of the CodePlex project, as used in the hostname of the CodePlex website.</param>
+		/// <param name="releaseName">The prefix of the releases to include, e.g. "Foo Editor" will match any release who's name starts "Foo Editor". An empty string will match all releases in the project.</param>
+		/// <param name="currentVersionReleaseDate">The data and time with which to compare.</param>
+		public CodePlexVersionCheck(ApplicationSettings settings, string projectName, string releaseName, DateTime currentVersionReleaseDate)
+			: this(projectName, releaseName, currentVersionReleaseDate) {
+			Settings = settings;
 		}
 
 		/// <summary>
 		/// Starts the check going in the background. <see cref="CheckComplete"/> will be called when the check is done.
 		/// </summary>
 		public void Check() {
-			var thread = new Thread(() => CheckThread());
-			thread.Start();
+			new Thread(CheckThread).Start();
 		}
 
-		void CheckThread() {
-			FetchAndProcess();
+		const long FileTimeAdjustment = 100000000;
 
-			if (null != CheckComplete) {
-				CheckComplete(this, new EventArgs());
+		void CheckThread() {
+			var lastCheck = new DateTime(2000, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+			if ((Settings != null) && (Settings.Integer["UpdateCheckTime"] != 0)) {
+				lastCheck = DateTime.FromFileTimeUtc((long)Settings.Integer["UpdateCheckTime"] * FileTimeAdjustment);
+			}
+
+			if ((Settings == null) || ((DateTime.Now - lastCheck).TotalDays > 1)) {
+				FetchAndProcess();
+				if ((Settings != null) && HasLatestVersion) {
+					Settings.Integer["UpdateCheckTime"] = (int)(DateTime.Now.ToFileTimeUtc() / FileTimeAdjustment);
+					Settings.Boolean["UpdateIsNew"] = IsNewVersion;
+					Settings.Integer["UpdateDate"] = (int)(LatestVersionDate.ToFileTimeUtc() / FileTimeAdjustment);
+					Settings.String["UpdateTitle"] = LatestVersionTitle;
+					Settings.String["UpdateUri"] = LatestVersionUri.ToString();
+				}
+			} else {
+				HasLatestVersion = true;
+				IsNewVersion = Settings.Boolean["UpdateIsNew"];
+				LatestVersionDate = DateTime.FromFileTimeUtc((long)Settings.Integer["UpdateDate"] * FileTimeAdjustment);
+				LatestVersionTitle = Settings.String["UpdateTitle"];
+				LatestVersionUri = new Uri(Settings.String["UpdateUri"]);
+			}
+
+			var checkComplete = CheckComplete;
+			if (checkComplete != null) {
+				checkComplete(this, new EventArgs());
 			}
 		}
 
@@ -77,9 +109,10 @@ namespace Jgr
 			if (null == item) return;
 
 			HasLatestVersion = true;
+			LatestVersionDate = DateTime.Parse(item.Element("pubDate").Value, CultureInfo.InvariantCulture);
 			LatestVersionTitle = item.Element("title").Value.Substring(item.Element("title").Value.IndexOf(": ", StringComparison.Ordinal) + 2);
 			LatestVersionUri = new Uri(item.Element("link").Value);
-			IsNewVersion = DateTime.Parse(item.Element("pubDate").Value, CultureInfo.InvariantCulture) > CurrentVersionReleaseDate;
+			IsNewVersion = LatestVersionDate > CurrentVersionReleaseDate;
 		}
 	}
 }
--- Tools/Tools.Designer.cs
+++ Tools/Tools.Designer.cs
@@ -35,6 +35,8 @@
 			this.imageListIcons = new System.Windows.Forms.ImageList(this.components);
 			this.listViewTools = new System.Windows.Forms.ListView();
 			this.labelTool = new System.Windows.Forms.Label();
+			this.buttonUpdate = new System.Windows.Forms.Button();
+			this.toolTip = new System.Windows.Forms.ToolTip(this.components);
 			this.SuspendLayout();
 			// 
 			// textBoxDescription
@@ -58,7 +60,7 @@
 			this.buttonLaunch.Enabled = false;
 			this.buttonLaunch.Location = new System.Drawing.Point(218, 377);
 			this.buttonLaunch.Name = "buttonLaunch";
-			this.buttonLaunch.Size = new System.Drawing.Size(150, 23);
+			this.buttonLaunch.Size = new System.Drawing.Size(200, 23);
 			this.buttonLaunch.TabIndex = 3;
 			this.buttonLaunch.Text = "Launch Tool";
 			this.buttonLaunch.UseVisualStyleBackColor = true;
@@ -87,7 +89,7 @@
 						| System.Windows.Forms.AnchorStyles.Left)));
 			this.listViewTools.Location = new System.Drawing.Point(12, 12);
 			this.listViewTools.Name = "listViewTools";
-			this.listViewTools.Size = new System.Drawing.Size(200, 388);
+			this.listViewTools.Size = new System.Drawing.Size(200, 359);
 			this.listViewTools.SmallImageList = this.imageListIcons;
 			this.listViewTools.TabIndex = 0;
 			this.listViewTools.UseCompatibleStateImageBehavior = false;
@@ -105,11 +107,22 @@
 			this.labelTool.Size = new System.Drawing.Size(354, 32);
 			this.labelTool.TabIndex = 1;
 			// 
+			// buttonUpdate
+			// 
+			this.buttonUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.buttonUpdate.Enabled = false;
+			this.buttonUpdate.Location = new System.Drawing.Point(12, 377);
+			this.buttonUpdate.Name = "buttonUpdate";
+			this.buttonUpdate.Size = new System.Drawing.Size(200, 23);
+			this.buttonUpdate.TabIndex = 5;
+			this.buttonUpdate.UseVisualStyleBackColor = true;
+			// 
 			// Tools
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(584, 412);
+			this.Controls.Add(this.buttonUpdate);
 			this.Controls.Add(this.labelTool);
 			this.Controls.Add(this.listViewTools);
 			this.Controls.Add(this.buttonExit);
@@ -133,6 +146,8 @@
 		private System.Windows.Forms.ImageList imageListIcons;
 		private System.Windows.Forms.ListView listViewTools;
 		private System.Windows.Forms.Label labelTool;
+		private System.Windows.Forms.Button buttonUpdate;
+		private System.Windows.Forms.ToolTip toolTip;
 	}
 }
 
